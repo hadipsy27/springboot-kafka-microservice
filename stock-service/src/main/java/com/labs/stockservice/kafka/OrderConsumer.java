1d3220d3064e3e808a4492ba83cc52ae85f56852
hadipsy27 <hadipsy27@gmail.com>
2023-11-14 15:00:31 +0700

    238. StockService Microservice - Create Kafka Consumer

---
+++ stock-service/src/main/java/com/labs/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,22 @@
+package com.labs.stockservice.kafka;
+
+import com.labs.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Consuming message %s", orderEvent.toString()));
+
+        // You can save the order event into the database
+
+    }
+
+}
