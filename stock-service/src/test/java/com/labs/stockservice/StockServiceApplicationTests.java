cf6ae9a143719e8154d57bbe483e61bab326946a
hadipsy27 <hadipsy27@gmail.com>
2023-11-14 10:17:27 +0700

    First commit

---
+++ stock-service/src/test/java/com/labs/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
